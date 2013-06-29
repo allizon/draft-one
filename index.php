b6dd311768ba9936bc347c949c271cf39b3fef12
mr-terrific <hey@allenholt.com>
2013-06-29 14:28:51 +0000

    Applying Oct 2012 updates - backbone.js, less, lots of JS rewriting

--- index.php
+++ index.php
@@ -1,63 +1,151 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Onward!</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
+	<title>DraftOne | Get your first draft out of your head!</title>
+	<link rel="stylesheet/less" type="text/css" href="css/style.less" />
 
-    <script type="text/javascript" src="http://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-        google.load( "jquery", "1.6" );
-    </script>
-    <script type="text/javascript" src="js/showdown.js"></script>
-    <script type="text/javascript" src="js/behaviors.js"></script>
-    <script type="text/javascript" src="js/onward.js"></script>
+	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 
+	<link href='http://fonts.googleapis.com/css?family=Coda:400,800' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript" src="js/less.min.js"></script>
+	<script type="text/javascript" src="js/showdown.js"></script>
+	<script type="text/javascript" src="js/modernizr.js"></script>
+	<script type="text/javascript" src="js/underscore-min.js"></script>
+	<script type="text/javascript" src="js/backbone-min.js"></script>
+	<script type="text/javascript" src="js/onward.js"></script>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-273478-15']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
 </head>
 
 <body>
 
-<div id="debug"></div>
-
-<header>
-    <h1>Onward!</h1>
-    <section id="action-buttons">
-        <a id="start" class="rounded-button">Go!</a>
-        <a id="pause" class="rounded-button">Pause</a>
-        <a id="stop" class="rounded-button active">Stop!</a>
-    </section>
-    <section class="clear"></section>
+<header title="Click the header to minimize it for more writing space!">
+	<h1>
+		DraftOne
+		<p>Keeping your writing feet<br>to the metaphorical fire.</p>
+	</h1>
+	<section class="clear"></section>
 </header>
 
-<section class="container clear">
-    <section id="stats">
-        <span class="label">Current word count: <span id="current-words">0</span></span>
-        <span class="label">Time elapsed: <span id="time-elapsed">0:00</span></span>
-    </section>
-
-	<section id="settings">
-        <h3 id="settings" class="hoverable"><a>Settings</a></h3>
-        <p>
-            <input type="radio" name="which" id="which-number-of-words" checked="checked" />
-            <span class="label">Number of words:</span> <input type="text" id="number-of-words" class="text" value="500" />
-            <br />
-            <input type="radio" name="which" id="which-number-of-minutes" />
-            <span class="label">or Number of minutes you want to write:</span> <input type="text" id="number-of-minutes" class="text" value="10" />
-        </p>
+<section id="header-smaller">
+	<section class="container">
+		DraftOne
 	</section>
-
-	<section id="textbox">
-		<textarea id="the-text" class="paused" disabled="true"></textarea>
-        <p>
-            <a href="javascript:;" id="export-as-html">Export as HTML</a>
-            |
-            <a href="javascript:;" id="export-as-text">Export as Text</a>
-        </p>
-	</section>
-
-
-    <section id="html" style="display:none;"></section>
-    <section id="close-template" class="close" style="display:none;"><a href="javascript:;">Close</a></section>
 </section>
+
+<section id="about">
+	<section class="container">
+		<h2><a id="open-about">Hey, so just what is this "DraftOne" thing, anyway? &raquo;</a></h2>
+		<p class="column">
+			Designed specifically to make you just keep moving forward when working
+			on <a href="http://nanowrimo.org" target="_blank">National Novel Writing Month</a>
+			&mdash; though you can use it any time, of course &mdash;
+			<strong>DraftOne</strong> gives you a quiet space in which to write but gently hassles you if
+			your fingers stop typing.
+			<br /><br />
+			DraftOne was inspired by <a href="http://writeordie.com/#Web+App" target="_blank">Dr. Wicked's Write or Die!</a>,
+			which helped me out tremendously during NaNoWriMo 2008.
+			I owe him a debt of gratitude for helping me get that thing done, and I hope that DraftOne can help you
+			get your own projects finished!
+		</p>
+		<p class="column">
+			Under the <strong>Settings</strong> menu, you can set a goal for yourself &mdash; either a target number of minutes or number of
+			words. Once you hit that goal, <strong>DraftOne</strong> will happily let you know that you've succeeded!
+			And if you need the extra help, you can even <em>turn off the backspace key</em> so that you have no choice
+			but to keep moving ahead.
+		</p>
+		<p class="clear center emphasis">
+			It's not about perfection &mdash; it's about getting that first draft <em>done</em>.
+		</p>
+		<p class="clear center">
+			<a id="close-about" href="#">Got it! Thanks! Now LET ME WRITE!</a>
+		</p>
+	</section>
+</section>
+
+<section class="app container clear">
+	<section id="stats">
+		<span class="label">Current word count: <span id="current-words">0</span></span>
+		<span class="label">Time elapsed: <span id="time-elapsed">0:00</span></span>
+	</section>
+
+	<section id="textbox" class="clearfix">
+		<section id="action-buttons">
+			<div class="leftcol">
+				<button id="start" class="rounded">Go!</button>
+				<button id="pause" class="rounded">Pause</button>
+				<button id="start-over" class="rounded">Start Over</button>
+			</div>
+			<div class="rightcol">
+				<button id="settings" class="hoverable rounded">Settings</button>
+			</div>
+		</section>
+
+		<textarea id="the-text" class="paused" disabled="true"></textarea>
+		<p class="left">
+			The editor allows <a href="http://daringfireball.net/projects/markdown/">Markdown formatting</a> if that's your thing.
+			|
+			<a href="javascript:;" id="export-as-text">Export as Formatted Text</a>
+			|
+			<a href="javascript:;" id="export-as-html">Export as Raw HTML</a>
+			<br>
+		</p>
+	</section>
+</section>
+
+<section id="goal-complete" style="display:none;">
+	<h3>Congratulations!</h3>
+	<p>You hit your goal! Well done, you!</p>
+	<button id="continue" class="rounded">Continue This Session</button>
+	<p class="emphasis">Remember to save your work<br />to a word processor or text editor!</p>
+	<!-- br />
+	<button id="start-new-session" class="rounded">Start New Session</button -->
+</section>
+
+<footer>
+	<section class="container">
+		<p class="left">
+		</p>
+		<p class="right">
+			&copy; 2012 Allen Holt. Follow me on <a href="http://twitter.com/allenholt/" target="_blank">Twitter</a> if you feel like it.<br />
+			Got feedback, comments or bug reports? <a href="mailto:ajholt@gmail.com?Subject=DraftOne">Send 'em my way.</a>
+		</p>
+	</section>
+</footer>
+
+<section id="settings">
+	<h3>Settings</h3>
+	<p>
+		<input type="radio" name="which" id="which-number-of-words" checked="checked">
+		<span class="label">Number of words:</span> <input type="text" id="number-of-words" class="text" value="500" />
+		<br>
+		<input type="radio" name="which" id="which-number-of-minutes" />
+		<span class="label">or Number of minutes you want to write:</span> <input type="text" id="number-of-minutes" class="text" value="10">
+	</p>
+	<p>
+		<input type="checkbox" name="disable-backspace" id="disable-backspace"> <span class="label">Disable backspacing/deleting</span><br>
+		<i>Disabling backspacing certainly makes you keep going forward, but can also be annoying as hell. Fair warning.</i>
+	</p>
+
+	<p>
+		<button id="save-settings">Save and Close</button>
+	</p>
+</section>
+
+<section id="html" style="display:none;"></section>
+<section id="close-template" class="close" style="display:none;"><button class="rounded">Close</button></section>
 
 </body>
 </html>
--- js/onward.js
+++ js/onward.js
@@ -1,170 +1,253 @@
-var time_not_typing = 0;
-var time_elapsed = 0;
-var interval_id;
-var paused = false;
-var is_done = false;
-
-function reset ( ) {
-	$( '#the-text' ).removeClass( 'warn' );
-	$( '#the-text' ).removeClass( 'annoy-1' );
-	$( '#the-text' ).removeClass( 'annoy-2' );
-	$( '#the-text' ).removeClass( 'annoy-3' );
-	$( '#the-text' ).removeClass( 'done' );
-	$( '#the-text' ).removeClass( 'paused' );
-}
-
-// I'd like to animate these transitions from one state to the next
-// rather than have them just immediately change color.
-//
-// Also, can we make this more object-oriented?
-
-function warn ( ) {
-	reset( );
-	$( '#the-text' ).addClass( 'warn' );
-}
-
-function annoy ( level ) {
-	reset( );
-	$( '#the-text' ).addClass( 'annoy-' + level );
-}
-
-function done ( ) {
-	reset( );
-	$( '#the-text' ).addClass( 'done' );
-	is_done = true;
-}
-
-function pause ( ) { 
-	if ( paused )
-	{
-		reset( );
-		paused = false;
-		interval_id = setInterval( updater, 1000 );
-		$( '#the-text' ).removeClass( 'paused' ).attr( 'disabled', false );
-	}
-	else
-	{
-		paused = true;
-		clearInterval( interval_id );
-		$( '#the-text' ).addClass( 'paused' ).attr( 'disabled', true );
-	}
-}
-
-function stop ( ) { 
-    reset( );
-    clearInterval( interval_id );
-}
-
-function wordCount ( ) {
-	var the_string = $( '#the-text' ).val( ).replace( '  ', ' ' );
-	var word_count = the_string.split( ' ' ).length;
-	$( '#current-words' ).text( word_count );
-	return word_count;
-}
-
-function timeElapsed ( ) {
-	++time_elapsed;
-	minutes = ( time_elapsed % 60 );
-	if ( minutes < 10 )
-		minutes = '0' + minutes;
-
-	time_elapsed_string = Math.floor( time_elapsed / 60 ) + ':' + minutes;
-	$( '#time-elapsed' ).text( time_elapsed_string );
-	return time_elapsed;
-}
-
-function updater ( ) {
-	++time_not_typing;
-	if ( time_not_typing >= 30)
-		annoy( 3 );
-	else if ( time_not_typing >= 20 )
-		annoy( 2 );
-	else if ( time_not_typing >= 15 )
-		annoy( 1 );
-	else if ( time_not_typing >= 10 )
-		warn( );
-
-	word_count = wordCount( );
-	time_elapsed = timeElapsed( );
-
-	if ( true == $( 'which-number-of-words' ).attr( 'checked' ) )
-	{
-		if ( word_count >= $( '#number-of-words' ).val( ) )
-			done( );
-	}
-	else
-	{
-		if ( time_elapsed >= $( '#number-of-minutes' ).val( ) * 60 )
-			done( );
-	}
+jQuery.fn.center = function ( ) {
+    this.css( "position", "absolute" );
+    this.css( "top", Math.max( 0, ( ( $(window).height() - this.outerHeight() ) / 2 ) + $(window).scrollTop() ) + "px" );
+    this.css( "left", Math.max( 0, ( ( $(window).width() - this.outerWidth() ) / 2 ) + $(window).scrollLeft() ) + "px" );
+    return this;
 }
 
 $( function ( ) {
+	var EditorView = Backbone.View.extend( {
+		el: '#textbox',
+		textarea: $( '#the-text' ),
+		time_elapsed_label: $( '#time-elapsed' ),
+		word_count_label: $( '#current-words' ),
+		goal_complete: $( '#goal-complete' ),
+		continue: $( 'button#continue' ),
+		start_new_session: $( 'button#start-new-session' ),
+		is_done: false,
+		is_paused: false,
+		interval_id: null,
+		time_not_typing: 0,
+		time_elapsed: 0,
+		interval_id: null,
+		paused: false,
+		is_done: false,
 
-    $( 'section#settings h3' ).click( function ( ) { 
-        $( 'section#settings p' ).slideToggle( );
-    } );
+		events: {
+			'click button#start'      : 'start',
+			'click button#pause'      : 'pause',
+			'click button#start-over' : 'start_over',
+			'keydown #the-text'       : 'keydown',
+			'editor:warn'             : 'warn',
+			'editor:annoy'            : 'annoy',
+			'editor:done'             : 'done',
+			'editor:autosave'         : 'autoSave'
+		},
 
-    $( '#action-buttons a' ).click( function ( ) { 
-        $( '#action-buttons a' ).removeClass( 'active' );
-        $( this ).addClass( 'active' );
-    } );
+		render: function ( ) {
+			this.time_elapsed_label.text( this.timeElapsedString( ) );
+			this.word_count_label.text( this.wordCount( ) );
+			return this;
+		},
+		reset: function ( ) {
+			this.textarea.removeClass( 'warn annoy-1 annoy-2 annoy-3 done paused' ).prop( 'disabled', false );
+		},
+		warn: function ( ) {
+			this.reset( );
+			this.textarea.addClass( 'warn' );
+		},
+		annoy: function ( e, level ) {
+			this.reset( );
+			this.textarea.addClass( 'annoy-' + level );
+		},
+		start: function ( ) {
+			this.is_paused = false;
+			clearInterval( this.interval_id ); // just in case
+			this.interval_id = setInterval( updater, 1000 );
+			this.reset( );
+		},
+		pause: function ( ) {
+			this.is_paused = true;
+			clearInterval( this.interval_id );
+			this.textarea.addClass( 'paused' ).prop( 'disabled', true );
+			this.autoSave( );
+		},
+		start_over: function ( ) {
+			this.reset( );
+			this.is_paused = false;
+			this.is_done = false;
+			this.pause( );
+			this.time_elapsed = 0;
+			this.textarea.val( '' );
+			this.render( );
+		},
+		done: function ( ) {
+			this.reset( );
+			this.pause( );
+			this.textarea.addClass( 'done' );
+			this.is_done = true;
+			this.goal_complete.show( ).center( );
+		},
+		keydown: function ( e ) {
+			var which = e.keyCode ? e.keyCode : e.which;
 
-	$( '#start' ).click( function ( ) {
-        if ( paused ) { 
-            pause( ); // unpauses - make unpause() function
-        }
+			switch ( which ) {
+				case 8:
+					if ( $( '#disable-backspace' ).prop( 'checked' ) ) {
+						return false;
+					}
+					break;
+				case 27:
+					this.pause( );
+					break;
+			}
 
-		interval_id = setInterval( updater, 1000 );
-		$( '#the-text' ).removeClass( 'paused' ).attr( 'disabled', false );
-	} );
+			if ( !this.is_paused && !this.is_done ) {
+				this.reset( );
+				this.time_not_typing = 0;
+			}
+		},
+		autoSave: function ( ) {
+			localStorage["text"] = this.textarea.val( );
+		},
+		wordCount: function ( ) {
+			var the_string = this.textarea.val( ).replace( '  ', ' ' );
+			return the_string.split( ' ' ).length;
+		},
+		timeElapsedString: function ( ) {
+			var minutes = ( this.time_elapsed % 60 );
+			if ( minutes < 10 ) {
+				minutes = '0' + minutes;
+			}
 
-	$( '#pause' ).click( function ( ) {
-		pause( );
-	} );
-
-	$( '#stop' ).click( function ( ) {
-		reset( );
-		clearInterval( interval_id );
-	} );
-
-	$( '#the-text' ).keydown( function ( e ) {
-		var which = e.keyCode ? e.keyCode : e.which;
-
-		switch ( which )
-		{
-			case 8:
-				return false;
-			case 27:
-				pause( );
-				break;
+			return Math.floor( this.time_elapsed / 60 ) + ':' + minutes;
 		}
-			
-		if ( !paused && !is_done )
-		{
-			reset( );
-			time_not_typing = 0;
+	} );
+
+
+	var editor_view = new EditorView( );
+
+	function updater ( ) {
+		editor_view.time_not_typing++;
+		editor_view.time_elapsed++;
+
+		if ( !editor_view.is_done ) {
+			if ( editor_view.time_not_typing >= 40 ) {
+				$( '#textbox' ).trigger( 'editor:annoy', 3 );
+			}
+			else if ( editor_view.time_not_typing >= 30 ) {
+				$( '#textbox' ).trigger( 'editor:annoy', 2 );
+			}
+			else if ( editor_view.time_not_typing >= 20 ) {
+				$( '#textbox' ).trigger( 'editor:annoy', 1 );
+			}
+			else if ( editor_view.time_not_typing >= 10 ) {
+				$( '#textbox' ).trigger( 'editor:warn' );
+			}
+
+			if ( true == $( '#which-number-of-words' ).prop( 'checked' ) ) {
+				if ( editor_view.wordCount( ) >= $( '#number-of-words' ).val( ) ) {
+					$( '#textbox' ).trigger( 'editor:done' );
+				}
+			} else {
+				if ( editor_view.time_elapsed >= $( '#number-of-minutes' ).val( ) * 60 ) {
+					$( '#textbox' ).trigger( 'editor:done' );
+				}
+			}
+		}
+
+		// autosave every 10 seconds
+		if ( 0 == editor_view.time_elapsed % 10 ) {
+			$( '#textbox' ).trigger( 'editor:autosave' );
+		}
+
+		editor_view.render( );
+	}
+
+	// ALl of these functiosn should get moved into the EditorView
+	$( 'button#settings' ).click( function ( ) {
+		$( 'section#settings' ).show( ).center( );
+		editor_view.pause( );
+	} );
+
+	$( '#open-about' ).click( function ( ) {
+		$( '#about' ).animate( { 'height': '400px' }, 500 );
+	} );
+
+	$( '#close-about' ).click( function ( ) {
+		$( '#about' ).animate( { 'height': '0px' }, 350 );
+		localStorage["hide_about"] = "true";
+	} );
+
+	$( '.close button' ).live( 'click', function ( ) {
+		$( '#html, #manuscript' ).hide( );
+	} );
+
+	$( '#continue' ).click( function ( ) {
+		editor_view.goal_complete.hide( );
+		editor_view.start( );
+	} );
+
+	$( 'header' ).click( function ( ) {
+		// $( '#about' ).hide( );
+		$( 'header' ).animate( { 'height': '0px' }, 500, function ( ) {
+			$( this ).hide( );
+			$( '#header-smaller' ).animate( { 'height': '48px' }, 350 );
+		} );
+
+		localStorage["small_header"] = 'true';
+	} );
+
+	$( '#header-smaller' ).click( function ( ) {
+		$( this ).animate( { 'height': '0px' }, 350, function ( ) {
+			$( 'header' ).show( ).animate( { 'height': '157px' } );
+		} );
+
+		localStorage["small_header"] = 'false';
+	} );
+
+	$( '#save-settings' ).click( function ( ) {
+		$( 'section#settings' ).hide( );
+		editor_view.start( );
+
+		if ( Modernizr.localstorage ) {
+			localStorage["min_words"] = $( '#number-of-words' ).val( );
+			localStorage["min_minutes"] = $( '#number-of-minutes' ).val( );
+			localStorage["disable_backspace"] = $( '#disable-backspace' ).prop( 'checked' ) ? 1 : 0;
+			editor_view.autoSave( );
 		}
 	} );
 
-	$( '#export-as-html' ).click( function ( ) {
-		var converter = new Showdown.converter( );
-		$( '#html' ).html( converter.makeHtml( $( '#the-text' ).val( ) ) );
-		$close = $( '#close-template' ).clone( ).show( ).attr( 'id', 'close' );
-		$( '#html' ).prepend( $close );
-		$( '#html' ).show( );
+	$( '#action-buttons button' ).click( function ( ) {
+		$( '#action-buttons button' ).removeClass( 'active' );
+		$( this ).addClass( 'active' );
 	} );
 
 	$( '#export-as-text' ).click( function ( ) {
 		var converter = new Showdown.converter( );
-		$( '#html' ).text( converter.makeHtml( $( '#the-text' ).val( ) ) );
+		$( '#html' ).center( ).html( converter.makeHtml( $( '#the-text' ).val( ) ) ).removeClass( 'monospace' );
 		$close = $( '#close-template' ).clone( ).show( ).attr( 'id', 'close' );
 		$( '#html' ).prepend( $close );
 		$( '#html' ).show( );
 	} );
 
-	$( '.close a' ).live( 'click', function ( ) { 
-		$( '#html, #manuscript' ).hide( );	
+	$( '#export-as-html' ).click( function ( ) {
+		var converter = new Showdown.converter( );
+		$( '#html' ).center( ).text( converter.makeHtml( $( '#the-text' ).val( ) ) ).addClass( 'monospace' );
+		$close = $( '#close-template' ).clone( ).show( ).attr( 'id', 'close' );
+		$( '#html' ).prepend( $close );
+		$( '#html' ).show( );
 	} );
-} );
 
+	if ( Modernizr.localstorage ) {
+		$( '#the-text' ).val( localStorage["text"] );
+		$( '#number-of-words' ).val( localStorage["min_words"] || 500 );
+		$( '#number-of-minutes' ).val( localStorage["min_minutes"] || 10 );
+
+		var prop_checked = ( 1 == localStorage["disable_backspace"] ) ? true : false;
+		$( '#disable-backspace' ).prop( 'checked', prop_checked );
+
+		if ( localStorage["small_header"] == 'true' ) {
+			$( 'header' ).hide( );
+			$( '#header-smaller' ).css( { 'height': '48px' } ).show( );
+		}
+
+		if ( localStorage["hide_about"] == 'true' ) {
+			$( '#about' ).hide( );
+		}
+	} else {
+		$( '#number-of-words' ).val( 500 );
+		$( '#number-of-minutes' ).val( 10 );
+		$( '#disable-backspace' ).prop( 'checked', false );
+	}
+} );
