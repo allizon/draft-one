b114ba4d542fbbf3ba73948a7e9a397f19c95f94
Allison Holt <github@alli.fyi>
2017-10-09 22:32:12 -0400

    Starting to work in Vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    this is my app
+  </div>
+</template>
+
+<script>
+// import Game from './Game.vue';
+
+export default {
+  name: 'app',
+
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+
+  components: {
+    // Game
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 require('../css/styles.sass');
+
+import Vue from 'vue';
+import App from './App.vue';
+
+var D1App = new Vue({
+  el: '#app'
+});
